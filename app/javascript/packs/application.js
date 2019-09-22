4922f5b5a6d367694e3faf379f1b833c23067a82
puppe1990 <matheus.puppe90@hotmail.com>
2019-09-22 15:55:43 -0300

    Add bootstrap and datatable

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+//= require jquery
+//= require bootstrap/dist/js/bootstrap
+//= require select2/dist/js/select2.full.js
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
