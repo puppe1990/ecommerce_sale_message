2900ebf3369bbfc9658637b81815e085c0104f6c
puppe1990 <matheus.puppe90@hotmail.com>
2020-05-22 11:26:03 -0300

    Add index2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,25 +1,25 @@
+# frozen_string_literal: true
+
 class OrdersController < ApplicationController
-    def index
-      @orders = list_orders
-    end
+  def index
+    @orders = list_orders
+  end  
 
-    private
-    def list_orders
-      uri = URI.parse("https://purchasestore.com.br/ws/wspedidos.json")
-      request = Net::HTTP::Get.new(uri)
-      request.content_type = "application/json"
-      request["Appkey"] = "ZTgyYjMzZDJhMDVjMTVjZWM4OWNiMGU5NjI1NTNkYmU="
-      request["Cache-Control"] = "no-cache"
+  def index2
+    @orders = list_orders2
+  end
 
-      req_options = {
-        use_ssl: uri.scheme == "https",
-      }
+  private
 
-      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-        http.request(request)
-      end
+  def list_orders
+    HTTParty.get('https://purchasestore.com.br/ws/wspedidos.json',
+                 headers: { content: 'application/json',
+                            Appkey: 'ZTgyYjMzZDJhMDVjMTVjZWM4OWNiMGU5NjI1NTNkYmU' })
+  end  
 
-      # response.code
-      JSON.parse(response.body)
-    end
+  def list_orders2
+    HTTParty.get('https://purchasestore.com.br/ws/wspedidos.json?page=2',
+                 headers: { content: 'application/json',
+                            Appkey: 'ZTgyYjMzZDJhMDVjMTVjZWM4OWNiMGU5NjI1NTNkYmU' })
+  end
 end
