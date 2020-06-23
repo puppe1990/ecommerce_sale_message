7fb3ccf88af8d006ee9720f266d09f49863a5a29
puppe1990 <matheus.puppe90@hotmail.com>
2020-06-23 20:46:51 -0300

    Lista 250 last orders from simplo7

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,24 +2,21 @@
 
 class OrdersController < ApplicationController
   def index
-    @orders = list_orders
-  end  
-
-  def index2
-    @orders = list_orders2
+    @send_orders = []
+    list_orders
   end
 
   private
 
   def list_orders
-    HTTParty.get('https://purchasestore.com.br/ws/wspedidos.json?page=1',
-                 headers: { content: 'application/json',
-                            Appkey: 'ZTgyYjMzZDJhMDVjMTVjZWM4OWNiMGU5NjI1NTNkYmU' })
-  end  
-
-  def list_orders2
-    HTTParty.get('https://purchasestore.com.br/ws/wspedidos.json?page=2',
-                 headers: { content: 'application/json',
-                            Appkey: 'ZTgyYjMzZDJhMDVjMTVjZWM4OWNiMGU5NjI1NTNkYmU' })
+    @orders = []
+    (1..5).each do |i|
+      @order_page = HTTParty.get("https://purchasestore.com.br/ws/wspedidos.json?page=#{i}",
+                                 headers: { content: 'application/json',
+                                            Appkey: 'ZTgyYjMzZDJhMDVjMTVjZWM4OWNiMGU5NjI1NTNkYmU' })
+      @order_page['result'].each do |order_page|
+        @orders << order_page
+      end
+    end
   end
 end
