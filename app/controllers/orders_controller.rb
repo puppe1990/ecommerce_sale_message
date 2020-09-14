44977ac86b6bd2580557a2f58912f05e0f90f1c2
puppe1990 <matheus.puppe90@hotmail.com>
2020-09-14 15:29:41 -0300

    decrease pages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@
 class OrdersController < ApplicationController
   def index
     @orders = []
-    (1..8).each do |i|
+    (1..4).each do |i|
       @order_page = HTTParty.get("https://purchasestore.com.br/ws/wspedidos.json?page=#{i}",
                                  headers: { content: 'application/json',
                                             Appkey: 'ZTgyYjMzZDJhMDVjMTVjZWM4OWNiMGU5NjI1NTNkYmU' })
